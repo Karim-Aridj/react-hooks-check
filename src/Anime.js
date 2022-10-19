894e1c5b52b9da5f17f780dfab47dedfffe35932
Karim-Aridj <abdelkarim.ardj@gmail.com>
2022-10-19 21:43:57 +0100

    First Commit

---
+++ src/Anime.js
@@ -0,0 +1,85 @@
+export const Anime = [
+    {
+        title: "Bleach: Sennen Kessen-hen ",
+        posterUrl: "https://gogocdn.net/cover/bleach-sennen-kessen-hen-1664387572.png",
+        rate: 5,
+        description: "Was it all just a coincidence, or was it inevitable? Ichigo Kurosaki gained the powers of a Soul Reaper through a chance encounter. As a Substitute Soul Reaper, Ichigo became caught in the turmoil of the Soul Society, a place where deceased souls gather. But with help from his friends, Ichigo overcame every challenge to become even stronger. "
+    },
+    {
+        title: "Spy x Family",
+        posterUrl: "https://gogocdn.net/cover/spy-x-family.png",
+        rate: 3,
+        description: "For the agent known as 'Twilight,' no order is too tall if it is for the sake of peace. Operating as Westalis' master spy, Twilight works tirelessly to prevent extremists from sparking a war with neighboring country Ostania. For his latest mission, he must investigate Ostanian politician Donovan Desmond by infiltrating his son's school: the prestigious Eden Academy. Thus, the agent faces the most difficult task of his career: get married, have a child, and play family. Twilight, or 'Loid Forger,' quickly adopts the unassuming orphan Anya to play the role of a six-year-old daughter and prospective Eden Academy student. For a wife, he comes across Yor Briar, an absent-minded office worker who needs a pretend partner of her own to impress her friends. However, Loid is not the only one with a hidden nature. Yor moonlights as the lethal assassin 'Thorn Princess.' For her, marrying Loid creates the perfect cover. Meanwhile, Anya is not the ordinary girl she appears to be; she is an esper, the product of secret experiments that allow her to read minds. Although she uncovers their true identities, Anya is thrilled that her new parents are cool secret agents! She would never tell them, of course. That would ruin the fun. Under the guise of 'The Forgers,' the spy, the assassin, and the esper must act as a family while carrying out their own agendas. Although these liars and misfits are only playing parts, they soon find that family is about far more than blood relations."
+    },
+    {
+    title: "Claymore",
+    posterUrl: "https://i.pinimg.com/736x/90/c9/f9/90c9f9bbf1d5bdb385d3898ff62862b7--claymore-manga-manga-anime.jpg",
+    rate: 5,
+    description: "Plot Summary:  In a world rife with deadly creatures called “youma”, a young silver eyed woman, Clare, works on behalf of an organisation that trains female youma halfbreeds into warriors with the ability to destroy these creatures. Considered a rogue for picking up a stray child & almost losing herself to her youma side by “Awakening”, she is constantly assigned… more In a world rife with deadly creatures called “youma”, a young silver eyed woman, Clare, works on behalf of an organisation that trains female youma halfbreeds into warriors with the ability to destroy these creatures. Considered a rogue for picking up a stray child & almost losing herself to her youma side by “Awakening”, she is constantly assigned rather dangerous missions "},
+    {
+    title: "Chainsaw Man",
+    posterUrl: "https://gogocdn.net/cover/chainsaw-man-1664388043.png",
+    rate: 3,
+    description: "Denji has a simple dream—to live a happy and peaceful life, spending time with a girl he likes. This is a far cry from reality, however, as Denji is forced by the yakuza into killing devils in order to pay off his crushing debts. Using his pet devil Pochita as a weapon, he is ready to do anything for a bit of cash. Unfortunately, he has outlived his usefulness and is murdered by a devil in contract with the yakuza. However, in an unexpected turn of events, Pochita merges with Denji's dead body and grants him the powers of a chainsaw devil. Now able to transform parts of his body into chainsaws, a revived Denji uses his new abilities to quickly and brutally dispatch his enemies. Catching the eye of the official devil hunters who arrive at the scene, he is offered work at the Public Safety Bureau as one of them. Now with the means to face even the toughest of enemies, Denji will stop at nothing to achieve his simple teenage dreams. "},
+    {
+        title: "Naruto Shippuden",
+        posterUrl: "https://i.pinimg.com/originals/8f/ee/98/8fee98fb2296d70ddd247a3a0195d7a1.jpg",
+        rate: 4,
+        description: "Naruto Shippuden is a continuation of original series Naruto. After 2 and a half years of training with his master, Naruto finally returns to his village of Konoha. Naruto is now older and more mature than before. With his new trained ability, his goal of searching for long lost friend, Uchiha Sasuke, is set for a stronger determination than before. Naruto Shippuden is a continuation of original series Naruto. After 2 and a half years of training with his master, Naruto finally returns to his village of Konoha. Naruto is now older and more mature than before. With his new trained ability, his goal of searching for long lost friend, Uchiha Sasuke, is set for a stronger determination than before. He is putting his ambitions to work along with the help of his many friends, though with so many obstacles ahead of him. Now, he will face even more dangerous enemies, especially the Akatsuki."},
+    {
+    title: "Death Note",
+    posterUrl: "https://i.pinimg.com/originals/58/fc/e3/58fce3b346bf7b55a5bf622901b1d0e4.jpg",
+    rate: 5,
+    description: "Yagami Light is an ace student with great aspects who’s bored out of his mind. One day he finds the Death Note, a notebook held by a shinigami (Death God). With the Death Note in hand, Light decides to create a perfect world. A world without crime or criminals. However when criminals start dropping dead one by one, the authorities send the legendar… more Yagami Light is an ace student with great aspects who’s bored out of his mind. One day he finds the Death Note, a notebook held by a shinigami (Death God). With the Death Note in hand, Light decides to create a perfect world. A world without crime or criminals. However when criminals start dropping dead one by one, the authorities send the legendary detective L to track down the killer."},
+    {
+    title: "Oussama Ranking",
+    posterUrl: "https://s1.zerochan.net/Ousama.Ranking.600.3467527.jpg",
+    rate: 5,
+    description: "The web manga centers around Bojji, a deaf, powerless prince who cannot even wield a children's sword. As the firstborn son, he strives hard and dreams of becoming the world's greatest king. However, people mutter about him behind his back as 'a good-for-nothing prince' and 'no way he can be king.' Bojji is able to make his first ever friend, 'Kage'(shadow)—a literal shadow on the ground who somehow understands Bojji well. (Kage is a survivor of the Kage assassin clan that was all but wiped out. No longer a killer, Kage now makes ends meet by stealing.) The story follows Bojji's coming-of-age as he meets various people in his life, starting with his fateful encounter with Kage."},
+    {
+        title: "Steins Gate",
+        posterUrl: "https://i.pinimg.com/originals/41/54/81/4154813026c70fe8674e2d1d46515450.png",
+        rate: 4,
+        description: "The story of Steins;Gate takes place in Akihabara and is about a group of friends who have managed to customize their microwave into a device that can send text messages to the past. As they perform different experiments, an organization named SERN who has been doing their own research on time travel tracks them down and now the characters have to find a way to avoid being captured by them."},
+    {
+    title: "Akam Ga Kill!",
+    posterUrl: "https://i.pinimg.com/originals/37/0d/b8/370db87083606d66ee4ce526f23620fd.jpg",
+    rate: 4,
+    description: "Tatsumi is a fighter who sets off to the Capital in search of a way to make money to assist his poverty-stricken village. After falling victim to a robbery by a woman and losing all his money, Tatsumi is taken in by a noble aristocratic girl by the name of Aria. In the following night, Aria's place is attacked by a group of assassins known as Night Raid. While Tatsumi at first tries to defend Aria from the assassin Akame, another member of the group stops the fight. She reveals that Aria has kidnapped several villagers and tortures them for fun. Shocked upon seeing the tortured villagers — including his two best friends, Tatsumi kills Aria. Seeing potential in Tatsumi, Night Raid invites the boy to their group warning that regardless of how they want to assassin corrupted people, they are murderers. Tatsumi accepts in order to become stronger and protect his village. In their fight against the Empire, Night Raid starts antagonizing an organization known as The Jaegars led by Esdeath, the most powerful fighter in the Empire. As both Night Raid and Jaegars suffer losses, the Empire forms a new secret police force, the Wild Hunt, with the son of the Prime Minister, Shura, as its leader, to bolster its law reinforcements, though Wild Hunt heavily abuses its new authority by killing innocent civilians for fun."},
+    {
+    title: "Boku No Hero Academia",
+    posterUrl: "https://gogocdn.net/cover/boku-no-hero-academia-6th-season-1664387814.png",
+    rate: 5,
+    description: "Midoriya Izuku was just a regular middle school student in a world where people with superpowers known as Quirks are the norm. However, he dreams of one day becoming a Hero, despite being bullied by his classmates for not having a Quirk. After being the only one to try and save his childhood friend, Katsuki, from a Villain, All Might, the world's greatest Hero, bestows upon him his own quirk, One For All. The story follows Izuku's entrance into Yuuei High School, a school that cultivates the next generation of Heroes."
+    },
+    {
+    title: "One Piece",
+    posterUrl: "https://gogocdn.net/images/anime/One-piece.jpg",
+    rate: 5,
+    description: "One Piece is a story about  Monkey D. Luffy, who wants to become a sea-robber. In a world mystical, there have a mystical fruit whom eat will have a special power but also have greatest weakness. Monkey ate Gum-Gum Fruit which gave him a strange power but he can NEVER swim. And this weakness made his dream become a sea  robber to find ultimate treasure is difficult. One Piece is a story about  Monkey D. Luffy, who wants to become a sea-robber. In a world mystical, there have a mystical fruit whom eat will have a special power but also have greatest weakness. Monkey ate Gum-Gum Fruit which gave him a strange power but he can NEVER swim. And this weakness made his dream become a sea  robber to find ultimate treasure is difficult. But along his ways, he meet himself many members to help. Together, they sail the Seven Seas of adventure in search of the elusive treasure “One Piece.”"
+    },
+    {
+    title: "Hunter X Hunter 2011",
+    posterUrl: "https://i.pinimg.com/736x/35/01/d5/3501d5bcf3ae8b1dadeac88887c6524d--hunter-fans-upcoming-anime.jpg",
+    rate: 5,
+    description: "Alternate retelling of the original Hunter x Hunter series. Gon Freece is a boy living on a small island. He learns that his fater who he doesn’t remember is an extremely famous man and an amazing hunter. Gon takes the hunte rexam to seek out his fater, but he soon learns that the challenges he must face will be beyond his imaginations."
+    },
+    {
+    title: "One Punch Man",
+    posterUrl: "https://i.pinimg.com/564x/88/a6/07/88a6073c1ae274e6f630ef57b6e52cfe.jpg",
+    rate: 5,
+    description: "In this new action-comedy, everything about a young man named Saitama screams 'AVERAGE,' from his lifeless expression, to his bald head, to his unimpressive physique. However, this average-looking fellow doesn't have your average problem... He's actually a superhero that's looking for tough opponents! The problem is, every time he finds a promising candidate he beats the snot out of them in one punch. Can Saitama finally find an evil villain strong enough to challenge him? Follow Saitama through his hilarious romps as he searches for new bad guys to challenge!"
+    },
+    {
+    title: "Jujutsu Kaizen",
+    posterUrl: "https://gogocdn.net/cover/jujutsu-kaisen-tv.png",
+    rate: 5,
+    description: "In a world where demons feed on unsuspecting humans, fragments of the legendary and feared demon Ryoumen Sukuna were lost and scattered about. Should any demon consume Sukuna's body parts, the power they gain could destroy the world as we know it. Fortunately, there exists a mysterious school of Jujutsu Sorcerers who exist to protect the precarious existence of the living from the undead! Yuuji Itadori is high schooler who spends his days visiting his bedridden grandfather. Although he looks like your average teenager, his immense physical strength is something to behold! Every sports club wants him to join, but Itadori would rather hang out with the school outcasts in the Occult Club. One day, the club manages to get their hands on a sealed cursed object, but little do they know the terror they'll unleash when they break the seal..."
+    },
+    {
+    title: "Caliphate",
+    posterUrl: "https://gogocdn.net/cover/kimetsu-no-yaiba.png",
+    rate: 5,
+    description: "Since ancient times, rumors have abounded of man-eating demons lurking in the woods. Because of this, the local townsfolk never venture outside at night. Legend has it that a demon slayer also roams the night, hunting down these bloodthirsty demons. For young Tanjirou, these rumors will soon to become his harsh reality... Ever since the death of his father, Tanjirou has taken it upon himself to support his family. Although their lives may be hardened by tragedy, they've found happiness. But that ephemeral warmth is shattered one day when Tanjirou finds his family slaughtered and the lone survivor, his sister Nezuko, turned into a demon. To his surprise, however, Nezuko still shows signs of human emotion and thought... Thus begins Tanjirou's quest to fight demons and turn his sister human again."
+    },
+];
